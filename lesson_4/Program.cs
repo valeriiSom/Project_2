67868b50b5ceb6d530febf2cfe58b7ccdba72221
ValeraSom <lerek02@mail.ru>
2023-09-14 21:30:47 +0300

    lesson_4.1

---
+++ lesson_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number");
+string number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+
+if(num1 % 7 == 0 && num1 % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
