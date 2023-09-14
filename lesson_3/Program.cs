dae52b00e036074c9ed6ce0d2af908e04bca2ada
ValeraSom <lerek02@mail.ru>
2023-09-14 21:17:17 +0300

    lesson_3.1

---
+++ lesson_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number");
+string number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+
+Console.Write("Enter number");
+string number2 = Console.ReadLine();
+int num2 = int.Parse(number2);
+
+int num3 = num1 % num2;
+
+if (num3 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine($"не кратно {num3}");
+}
