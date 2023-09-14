5e46d55665cfbf4646866e34c6f8ff2c5a19d108
ValeraSom <lerek02@mail.ru>
2023-09-14 20:56:30 +0300

    lisson_2.2

--- lesson_2/Program.cs
+++ lesson_2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int num1 = num / 100;
+int num2 = num  %10;
+
+int rez = num1 * 10 + num2;
+
+Console.WriteLine(rez);
