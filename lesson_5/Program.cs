d702fcaac3ec68c1d020c33820a271d185b0a94d
ValeraSom <lerek02@mail.ru>
2023-09-14 21:42:44 +0300

    lesson_5.1

---
+++ lesson_5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number");
+string number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+
+Console.Write("Enter number");
+string number2 = Console.ReadLine();
+int num2 = int.Parse(number2);
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
