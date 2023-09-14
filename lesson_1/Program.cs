3289f9263c01bd65f515f17197413663f14d2477
ValeraSom <lerek02@mail.ru>
2023-09-14 20:56:59 +0300

    lesson_1.2

---
+++ lesson_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10,100);
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+if(num1 > num2 )
+{
+    Console.WriteLine($" max = {num1}");
+}
+else
+{
+    Console.WriteLine(" max = " + num2);
+}
